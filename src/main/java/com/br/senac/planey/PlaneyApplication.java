bc0ea548a4d694a547f41b5f004dfa5e03fe336e
Delaine <delainelynn@hotmail.com>
2022-08-28 13:46:49 -0300

    feat(project): First Commit
    Creating initial structure of the project

---
+++ src/main/java/com/br/senac/planey/PlaneyApplication.java
@@ -0,0 +1,13 @@
+package com.br.senac.planey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/senac/planey/PlaneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senac.planey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
